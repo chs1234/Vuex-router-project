1dddb917ef4ab53025f81c7613acb9211f2c8ad8
chs1234 <chs4413@gmail.com>
2020-03-08 16:39:03 +0900

    login with token in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import router from "../router/index.js";
+import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -35,19 +36,48 @@ export default new Vuex.Store({
   },
   actions: {
     // 로그인 시도
-    login({ state, commit }, loginObj) {
-      let selectedUser = null;
+    login({ commit }, loginObj) {
+      axios.post('https://reqres.in/api/login', loginObj) // 두번째 인자는 파라미터(body)
+      .then(res => {
+        // 성공 시 token(실제로는 user id 값을 받아옴.)
+        // 토큰을 헤더에 포함시켜서 유저 정보를 요청
+        let token = res.data.token;
+        let config = {
+          headers: {
+            "access-token": token
+          }
+        }
+        axios.get('https://reqres.in/api/users/2', config)
+          .then(response => {
+            let userInfo = {
+              id: response.data.data.id,
+              first_name: response.data.data.first_name,
+              last_name: response.data.data.last_name,
+              avatar: response.data.data.avatar
+            };
 
-      state.allUsers.forEach(user => {
-        if (user.email === loginObj.email) selectedUser = user;
+            commit('loginSuccess', userInfo);
+          })
+          .catch(error => {
+            alert("이메일과 비밀번호를 확인해주세요.");
+          });
+      })
+      .catch(err => {
+        alert("이메일과 비밀번호를 확인해주세요.");
       });
 
-      if (selectedUser === null || selectedUser.password !== loginObj.password)  
-        commit('loginError')
-      else {
-        commit('loginSuccess', selectedUser);
-        router.push({name: 'mypage'});
-      }
+      // let selectedUser = null;
+
+      // state.allUsers.forEach(user => {
+      //   if (user.email === loginObj.email) selectedUser = user;
+      // });
+
+      // if (selectedUser === null || selectedUser.password !== loginObj.password)  
+      //   commit('loginError')
+      // else {
+      //   commit('loginSuccess', selectedUser);
+      //   router.push({name: 'mypage'});
+      // }
     },
 
     logout({ commit }) {
--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>
-            {{ userInfo.name }}님 환영합니다.
+            {{ userInfo.first_name }} {{ userInfo.last_name }}님 환영합니다.
         </h1>
     </div>
 </template>
